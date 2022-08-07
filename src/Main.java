4f9b537f4aeb7ea74921e9bd3f1b6556da293a88
IgorBerger03 <igor.berger98@yandex.ru>
2022-08-07 13:38:14 +0500

    HW 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
+        Author publisher1 = new Author("Джек","Лондон");
+        Author publisher2 = new Author("Стивен", "Кинг");
+        Book book1 = new Book("Мартин Иден", publisher1, 1909);
+        Book book2 = new Book("Сияние", publisher2, 1977);
+        book1.setYear(1910);
+        book2.setYear(1980);
+        System.out.println("Название:" + book1.getTitle() + ", " + "Автор:" + publisher1 + ", " + "Год:" + book1.getYear());
+        System.out.println("Название:" + book2.getTitle() + ", " + "Автор:" + publisher2 + ", " + "Год:" + book2.getYear());
+        System.out.println("Измененный год публикации:" + book1.getYear());
+        System.out.println("Измененный год публикации:" + book2.getYear());
 
     }
 }
